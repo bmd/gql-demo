a1fcd41cc65f412c29fbe4cc671cd26fc2a0020b
Brendan Downing <brendanmdowning@gmail.com>
2020-02-17 12:40:45 -0500

    Build employee and dept models

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import { ENV } from './config'
+import moment from 'moment';
 const { ApolloServer, gql } = require('apollo-server');
 const Knex = require('knex');
+
+const sql2epoch = (ts) => moment(ts, 'YYYY-MM-DDTHH:MM:SS.SSSZ').unix()
 
 const conn = Knex({
   client: 'mysql',
   useNullAsDefault: true,
+  log: true,
   connection: {
     host: ENV.DB_HOST,
     database: ENV.DB_NAME,
@@ -16,18 +20,11 @@ const conn = Knex({
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
   type Department {
     id: String
     name: String
     manager: Employee
+    employees: [Employee]
   }
 
   type Employee {
@@ -36,48 +33,74 @@ const typeDefs = gql`
     lastName: String
     gender: String
     hireDate: Int
+    birthDate: Int
     department: Department
+    title: String
+    currentSalary: Int
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
     departments: [Department]
     department(id: String!): Department
-    employees: [Employee]
+    employees(limit: Int!, offset: Int!): [Employee]
+    employee(id: Int!): Employee
   }
 `;
 
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
-
 const resolvers = {
   Query: {
-    books: () => books,
     departments: async () => await conn.select().from('departments').orderBy('dept_no', 'asc'),
     department: async(_, { id }) => await conn.select().from('departments').where('dept_no', id).first(),
+    employees: async (_, { limit, offset }) => conn.select().from('v_full_employees').limit(limit).offset(offset),
+    employee: async (_, { id }) => await conn.select().from('v_full_employees').where('emp_no', id).first(),
+  },
+  Employee: {
+    id: (obj) => obj.emp_no,
+    firstName: (obj) => obj.first_name,
+    lastName: (obj) => obj.last_name,
+    hireDate: (obj) => sql2epoch(obj.hire_date),
+    birthDate: (obj) => sql2epoch(obj.birth_date),
+    title: async (obj) => {
+      const t = await conn.select()
+        .from('titles')
+        .where('emp_no', obj.emp_no)
+        .orderBy('from_date', 'desc')
+        .first();
+
+      return t.title;
+    },
+    currentSalary: async (obj) => {
+      const s = await conn.select()
+        .from('salaries')
+        .where('emp_no', obj.emp_no)
+        .orderBy('from_date', 'desc')
+        .first();
+
+      return s.salary;
+    },
+    department: async (obj) => {
+      const d = await conn.select()
+        .from('departments')
+        .join('current_dept_emp', 'departments.dept_no', 'current_dept_emp.dept_no')
+        .where('emp_no', obj.emp_no)
+        .first();
+
+      return d;
+    }
   },
   Department: {
     id: (obj) => obj.dept_no,
     name: (obj) => obj.dept_name,
     manager: async (obj) => {
-      const x = await conn.select()
+      return await conn.select()
         .from('dept_manager')
-        .join('employees', 'employees.emp_no', 'dept_manager.emp_no')
+        .join('v_full_employees', 'v_full_employees.emp_no', 'dept_manager.emp_no')
         .where(conn.raw('to_date >= NOW()'))
-        .andWhere('dept_no', obj.dept_no).first();
-      console.log(x)
-      return x
+        .andWhere('dept_no', obj.dept_no)
+        .first();
+    },
+    employees: async (obj) => {
+      return await conn.select()
     }
   }
 };
